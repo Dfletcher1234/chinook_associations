89090b91217654e260a5aeea1032dab7dcfc67cd
David FletcherE <dtsfletcher@gmail.com>
2017-09-10 17:54:18 -0400

    DONE

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ApplicationRecord
   belongs_to :artist
   has_many :tracks
+  has_and_belongs_to_many :playlists, through: :artist
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,8 @@
 class Artist < ApplicationRecord
   has_many :albums
+  has_many :tracks, through: :albums
+  has_and_belongs_to_many :media_types
+
+
+
 end
